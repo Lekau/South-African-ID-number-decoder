7da62eaae92e2d4069a2f732296c5a72eaa11ba2
Lekau Mamabolo <lekau9222@gmail.com>
2020-04-20 04:41:14 +0200

    Done after understanding static vars and pointers

--- ft_idnumber.c
+++ ft_idnumber.c
@@ -58,74 +58,57 @@ char * ft_gender(char unit)
     char *gender;
 
     if (unit >= '5' && unit <= '9')
-    {
         gender = "This person is Male \n";
-    }
     else 
-    {
         gender = "This person is Female \n";
-    }
+
     return(gender);
 }
 
-char * ft_citizen(char unit)
+char *ft_citizen(char unit)
 {
-    char *c_status;
+    char *here = "This person was born in South Africa.\n";
+    char *not_here = "This person was not born in South Africa.\n";
     if (unit == '0')
     {
-        c_status = "This person was born in South Africa.\n";
+        return(here);
     }
     else
-    {
-        c_status = "This person was not born in South Africa.\n";
-    }
-    return (c_status);
+        return(not_here);
 }
 
-char * ft_month(char m1, char m2)
+/* char *ft_month(char m1, char m2)
 {
-    char str_month[12][12] = {"January", "Februay", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"};
+    char *str_month[12] = {"January", "Februay", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"};
     int num_month;
     char char_month[2] = {m1, m2};
 
     num_month = ft_atoi(char_month);
 
-    return (*str_month[num_month]);
-}
+    return (str_month[num_month]);
+} */
 
 char * ft_date_of_birth(char c1, char c2, char m1, char m2, char d1, char d2)
 {
-    char *date_of_birth;
-    char message[] = "This person was born: ";
-    char *str_month;
-    int i;
-    int k;
-
-    k = 0;
-    i = 0;
-    str_month = ft_month(m1, m2);
-
-    date_of_birth = (char*)malloc(sizeof(char) * (ft_strlen(str_month) + 30));
-    while (message[i])
-    {
-        date_of_birth[i] = message[i];
-        i++;
-    }
-    date_of_birth[i] = d1;
-    date_of_birth[i++] = d2;
-    date_of_birth[i++] = ' ';
-
-    while(str_month[k])
-    {
-        str_month[k] = date_of_birth[i];
-        i++;
-        k++;
-    }
-    date_of_birth[i] = ' ';
-    date_of_birth[i++] = '1';
-    date_of_birth[i++] = '9';
+    static char date_of_birth[21];
+    char str_month[][13] = {"January", "Februay", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"};
+    char char_month[2] = {m1, m2};
+    int num_month= ft_atoi(char_month);
+    int i = 0, k = 0;
+    
     date_of_birth[i++] = d1;
     date_of_birth[i++] = d2;
+    date_of_birth[i++] = ' ';
+    
+    while(str_month[num_month - 1][k] != '\0')
+        date_of_birth[i++] = str_month[num_month - 1][k++];
+    
+    date_of_birth[i++] = ' ';
+    date_of_birth[i++] = '1';
+    date_of_birth[i++] = '9';
+    date_of_birth[i++] = c1;
+    date_of_birth[i++] = c2;
+    date_of_birth[i] = '\0';
 
     return (date_of_birth);
 }
@@ -137,13 +120,14 @@ int main(int counter, char **vector)
         ft_putstr("For the ID Number: ");
         ft_putstr(vector[1]);
         ft_putchar('\n');
-        ft_putstr(ft_date_of_birth(vector[1][0], vector[1][1], vector[1][2], vector[1][3], vector[1][4], vector[1][5]));
         ft_putstr(ft_gender(vector[1][6]));
-        ft_putstr(ft_citizen(vector[1][10]));
+        ft_putstr(ft_citizen(vector[1][10])); 
+        ft_putstr("This Person's date of birth is: ");
+        ft_putstr(ft_date_of_birth(vector[1][0], vector[1][1], vector[1][2], vector[1][3], vector[1][4], vector[1][5]));
     }
     else 
     {
-        ft_putstr("Please on enter one argument.");
+        ft_putstr("Please on enter an your ID number only.");
     }
     return (0);
 }
